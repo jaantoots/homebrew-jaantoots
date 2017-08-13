d18bfc6a17384137f827da89728cabc3c8344d4f
Jaan Toots <jaantoots@gmail.com>
2017-08-13 20:56:00 +0300

    git-annex-remote-rclone 0.6

--- Formula/git-annex-remote-rclone.rb
+++ Formula/git-annex-remote-rclone.rb
@@ -1,8 +1,8 @@
 class GitAnnexRemoteRclone < Formula
   desc "Use rclone supported cloud storage with git-annex"
   homepage "https://github.com/DanielDent/git-annex-remote-rclone"
-  url "https://github.com/DanielDent/git-annex-remote-rclone/archive/v0.4.tar.gz"
-  sha256 "c67ee42d3ab81bf7b471f8b46cce58ae9bb39393ea93630fb7909c9c5c79b727"
+  url "https://github.com/DanielDent/git-annex-remote-rclone/archive/v0.6.tar.gz"
+  sha256 "fb9bb77c6dd30dad4966926af87f63be92ef442cfeabcfd02202c657f40439d0"
   head "https://github.com/DanielDent/git-annex-remote-rclone.git"
 
   bottle :unneeded
